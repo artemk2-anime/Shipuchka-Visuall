465f5728e423e9094cb5bc32cf1a3e0f5dd2f75c
artemk2-anime <pvpezz896@gmail.com>
2026-02-02 23:40:24 +0500

    Create ShipuchkaVisual.java

---
+++ src/main/resources/src/main/java/ru/shipuchka/visual/ShipuchkaVisual.java
@@ -0,0 +1,11 @@
+package ru.shipuchka.visual;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ShipuchkaVisual implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Shipuchka Visual loaded!");
+    }
+}
